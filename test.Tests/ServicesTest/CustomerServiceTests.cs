8dd5bdd65dee667e6fdcd40eb81b37c9a3ed8381
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-09-10 10:12:18 -0500

    test

--- test.Tests/ServicesTest/CustomerServiceTests.cs
+++ test.Tests/ServicesTest/CustomerServiceTests.cs
@@ -13,7 +13,7 @@ namespace test.Tests.ServicesTest
         public void CustomerService_GetCustomer_ReturnUsersList() {
             //Arrange -varibles, clases, mocks
             var customerService = new CustomerService();
-            var expectedResult = new List<string> { "Jhoan" };
+            var expectedResult = new List<string> { "Jhoa" };
 
             //Act
             var customers = customerService.GetCustomers("Jhoan");
