fb97a9f023b683e937ecdc8f47810a2c8e74fbe9
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-09-10 10:09:49 -0500

    ehbjcebe

---
+++ test.Tests/ServicesTest/CustomerServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using test.Services;
+namespace test.Tests.ServicesTest
+{
+    public class CustomerServiceTests
+    {
+        [Fact]
+        public void CustomerService_GetCustomer_ReturnUsersList() {
+            //Arrange -varibles, clases, mocks
+            var customerService = new CustomerService();
+            var expectedResult = new List<string> { "Jhoan" };
+
+            //Act
+            var customers = customerService.GetCustomers("Jhoan");
+
+            //Assert
+            customers.Should().Equal(expectedResult);
+        }
+    }
+}
---
+++ test/Services/CustomerService.cs
@@ -0,0 +1,13 @@
+﻿namespace test.Services
+{
+    public class CustomerService
+    {
+        private List<string> customers = new List<string> { "Jhoan", "Jonathan", "Santiago" };
+
+        public ICollection<string> GetCustomers(string name) {
+            return customers.Where(customers => customers == name).ToList();
+        }
+
+    }
+}
+
