7d201dde4dd8d6ebe3eb5496e1d783c7b06e9f0d
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-09-11 16:05:32 -0500

    kndksdk

--- test/Controllers/WeatherForecastController.cs
+++ test/Controllers/WeatherForecastController.cs
@@ -26,7 +26,7 @@ namespace test.Controllers
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)],
-                Version = "version"
+                Version = "version111"
             })
             .ToArray();
         }
