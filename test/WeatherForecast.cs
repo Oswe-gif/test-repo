2b6f5eef9b4d3d0c9ec254a877eca205b6266c9a
Jhoan Oswaldo Ome Vega <Jhoan-Oswaldo.Ome@endava.com>
2024-09-11 15:53:14 -0500

    dknksdks

--- test/WeatherForecast.cs
+++ test/WeatherForecast.cs
@@ -9,5 +9,6 @@ namespace test
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
+        public string Version { get; set; }
     }
 }
